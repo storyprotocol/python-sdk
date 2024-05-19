c0eaf087df21db4f8158595147a00dedb78b7cd2
Andrew Chung <aandrewchung@gmail.com>
2024-05-18 22:01:36 -0700

    Created integration tests for register derivative with license tokens

--- tests/integration/test_integration_ip_asset.py
+++ tests/integration/test_integration_ip_asset.py
@@ -33,6 +33,8 @@ def story_client():
 # def test_register_ip_asset(story_client):
 #     token_id = get_token_id(MockERC721, story_client.web3, story_client.account)
 
+#     print(token_id)
+
 #     response = story_client.IPAsset.register(
 #         token_contract=MockERC721,
 #         token_id=token_id
@@ -42,47 +44,52 @@ def story_client():
 #     assert 'ipId' in response
 #     assert response['ipId'] is not None
 
-#     # print(response['ipId'])
+#     print(response['ipId'])
 
-def test_registerDerivative(story_client):
-    parent_token_id = get_token_id(MockERC721, story_client.web3, story_client.account)
+# def test_registerDerivative(story_client): #can only run once since using preset variables
+#     parent_ip_id = "0x567603411Fb957759Ac2090659B73cC5f099456D"
     
-    parent_ip_response = story_client.IPAsset.register(
-        token_contract=MockERC721,
-        token_id=parent_token_id
-    )
-    assert parent_ip_response is not None
-    assert 'ipId' in parent_ip_response
-    assert parent_ip_response['ipId'] is not None
-    parent_ip_id = parent_ip_response['ipId']
-    
-    # Attach license terms to parent IP
-    license_template = "0x260B6CB6284c89dbE660c0004233f7bB99B5edE7"  # Replace with actual license template address
-    no_commercial_license_terms_id = 2  # Replace with actual license terms ID
+#     # Attach license terms to parent IP
+#     license_template = "0x260B6CB6284c89dbE660c0004233f7bB99B5edE7"  # Replace with actual license template address
+#     no_commercial_license_terms_id = 2  # Replace with actual license terms ID
 
-    attach_license_response = story_client.License.attachLicenseTerms(
-        ip_id=parent_ip_id,
-        license_template=license_template,
-        license_terms_id=no_commercial_license_terms_id
-    )
-    assert attach_license_response is not None
-    assert 'txHash' in attach_license_response
-    assert attach_license_response['txHash'] is not None
+#     attach_license_response = story_client.License.attachLicenseTerms(
+#         ip_id=parent_ip_id,
+#         license_template=license_template,
+#         license_terms_id=no_commercial_license_terms_id
+#     )
+#     assert attach_license_response is not None
+#     assert 'txHash' in attach_license_response
+#     assert attach_license_response['txHash'] is not None
 
-    child_token_id = get_token_id(MockERC721, story_client.web3, story_client.account)
+#     child_ip_id = "0xc478561B1A18331d92b2b6f6a863dbAA874729c3"
+#     assert child_ip_id is not None
+
+#     # Register derivative IP
+#     response = story_client.IPAsset.registerDerivative(
+#         child_ip_id=child_ip_id,
+#         parent_ip_ids=[parent_ip_id],
+#         license_terms_ids=[no_commercial_license_terms_id],
+#         license_template=license_template
+#     )
     
-    child_ip_response = story_client.IPAsset.register(
-        token_contract=MockERC721,
-        token_id=child_token_id
-    )
-    child_ip_id = child_ip_response['ipId']  # Replace with actual child IP ID
+#     assert response is not None
+#     assert 'txHash' in response
+#     assert response['txHash'] is not None
+#     assert isinstance(response['txHash'], str)
+#     assert len(response['txHash']) > 0
+
+def test_registerDerivativeWithLicenseTokens(story_client): #can only run once since using preset variables
+
+    child_ip_id = "0xDd4330c5aeA6ab3a3E9620D2c74799bAb3BD117D"  #make sure the child ip has access to these token ids
+    assert child_ip_id is not None
+
+    licenseTokenIds = [1199]
 
     # Register derivative IP
-    response = story_client.IPAsset.registerDerivative(
+    response = story_client.IPAsset.registerDerivativeWithLicenseTokens(
         child_ip_id=child_ip_id,
-        parent_ip_ids=[parent_ip_id],
-        license_terms_ids=[no_commercial_license_terms_id],
-        license_template=license_template
+        license_token_ids=licenseTokenIds
     )
     
     assert response is not None

7008a87f6cb1f31de13b97881d0189362a337a4b
Andrew Chung <aandrewchung@gmail.com>
2024-05-21 11:48:30 -0700

    Renamed package name to have underscores

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='story-protocol-python-sdk',
+    name='story_protocol_python_sdk',
     version='0.1.0',
     packages=find_packages(where='src', exclude=["tests"]),
     package_dir={'': 'src'},

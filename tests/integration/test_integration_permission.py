96d335e35bc0eca83cd1fcbc2babed19ed051e07
Andrew Chung <aandrewchung@gmail.com>
2024-05-31 13:56:18 -0700

    Cleaned up integration test for permission client

--- tests/integration/test_integration_permission.py
+++ tests/integration/test_integration_permission.py
@@ -30,22 +30,6 @@ def story_client():
     return get_story_client_in_sepolia(web3, account)
 
 def test_setPermission(story_client):
-    config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'src', 'story_protocol_python_sdk', 'scripts', 'config.json'))
-    with open(config_path, 'r') as config_file:
-        config = json.load(config_file)
-    contract_address = None
-    for contract in config['contracts']:
-        if contract['contract_name'] == 'AccessController':
-            contract_address = contract['contract_address']
-            break
-    if not contract_address:
-        raise ValueError(f"Contract address for AccessController not found in config.json")
-    abi_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'src', 'story_protocol_python_sdk', 'abi', 'AccessController', 'AccessController.json'))
-    with open(abi_path, 'r') as abi_file:
-        abi = json.load(abi_file)
-
-    contract = web3.eth.contract(address=contract_address, abi=abi)
-
     token_id = get_token_id(MockERC721, story_client.web3, story_client.account)
 
     response = story_client.IPAsset.register(

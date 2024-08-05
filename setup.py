3f305f38e36054aa335162388a4a6ada83f8a0ef
Andrew  Chung <andrewchung@Andrews-MacBook-Pro-2.local>
2024-08-05 14:38:22 -0700

    Updated to 15 parent protocol changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='story_protocol_python_sdk',
-    version='0.3.2',
+    version='0.3.3',
     packages=find_packages(where='src', exclude=["tests"]),
     package_dir={'': 'src'},
     install_requires=[

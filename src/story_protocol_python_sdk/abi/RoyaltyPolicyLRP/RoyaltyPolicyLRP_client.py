0a3ae7e9f86146b781d145423572b8f5f05b0c2a
Andrew Chung <aandrewchung@gmail.com>
2025-01-15 20:26:00 -0800

    Updated auto generated classes to 1.3

--- src/story_protocol_python_sdk/abi/RoyaltyPolicyLRP/RoyaltyPolicyLRP_client.py
+++ src/story_protocol_python_sdk/abi/RoyaltyPolicyLRP/RoyaltyPolicyLRP_client.py
@@ -3,7 +3,7 @@ import json
 import os
 from web3 import Web3
 
-class LicenseRegistryClient:
+class RoyaltyPolicyLRPClient:
     def __init__(self, web3: Web3):
         self.web3 = web3
         # Assuming config.json is located at the root of the project
@@ -12,12 +12,12 @@ class LicenseRegistryClient:
             config = json.load(config_file)
         contract_address = None
         for contract in config['contracts']:
-            if contract['contract_name'] == 'LicenseRegistry':
+            if contract['contract_name'] == 'RoyaltyPolicyLRP':
                 contract_address = contract['contract_address']
                 break
         if not contract_address:
-            raise ValueError(f"Contract address for LicenseRegistry not found in config.json")
-        abi_path = os.path.join(os.path.dirname(__file__), '..', '..', 'abi', 'jsons', 'LicenseRegistry.json')
+            raise ValueError(f"Contract address for RoyaltyPolicyLRP not found in config.json")
+        abi_path = os.path.join(os.path.dirname(__file__), '..', '..', 'abi', 'jsons', 'RoyaltyPolicyLRP.json')
         with open(abi_path, 'r') as abi_file:
             abi = json.load(abi_file)
         self.contract = self.web3.eth.contract(address=contract_address, abi=abi)

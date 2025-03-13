e89755d02fb99cdc6ba1d8a4985c47fd6b7f25ec
Andrew Chung <aandrewchung@gmail.com>
2025-03-13 16:25:56 +0900

    Updated ip asset integration test to use classes

--- tests/integration/test_integration_ip_asset.py
+++ tests/integration/test_integration_ip_asset.py
@@ -21,116 +21,116 @@ from setup_for_integration import (
     PIL_LICENSE_TEMPLATE
 )
 
-# class TestIPAssetRegistration:
-#     @pytest.fixture(scope="module")
-#     def child_ip_id(self, story_client):
-#         token_id = get_token_id(MockERC721, story_client.web3, story_client.account)
+class TestIPAssetRegistration:
+    @pytest.fixture(scope="module")
+    def child_ip_id(self, story_client):
+        token_id = get_token_id(MockERC721, story_client.web3, story_client.account)
 
-#         response = story_client.IPAsset.register(
-#             nft_contract=MockERC721,
-#             token_id=token_id
-#         )
+        response = story_client.IPAsset.register(
+            nft_contract=MockERC721,
+            token_id=token_id
+        )
 
-#         assert 'txHash' in response
-#         assert isinstance(response['txHash'], str)
+        assert 'txHash' in response
+        assert isinstance(response['txHash'], str)
 
-#         assert response is not None
-#         assert 'ipId' in response
-#         assert response['ipId'] is not None
-#         return response['ipId']
+        assert response is not None
+        assert 'ipId' in response
+        assert response['ipId'] is not None
+        return response['ipId']
 
-#     def test_register_ip_asset(self, story_client, child_ip_id):
-#         assert child_ip_id is not None
+    def test_register_ip_asset(self, story_client, child_ip_id):
+        assert child_ip_id is not None
 
-#     def test_register_ip_asset_with_metadata(self, story_client):
-#         token_id = get_token_id(MockERC721, story_client.web3, story_client.account)
-#         metadata = {
-#             'ip_metadata_uri': "test-uri",
-#             'ip_metadata_hash': web3.to_hex(web3.keccak(text="test-metadata-hash")),
-#             'nft_metadata_uri': "test-nft-uri",
-#             'nft_metadata_hash': web3.to_hex(web3.keccak(text="test-nft-metadata-hash"))
-#         }
+    def test_register_ip_asset_with_metadata(self, story_client):
+        token_id = get_token_id(MockERC721, story_client.web3, story_client.account)
+        metadata = {
+            'ip_metadata_uri': "test-uri",
+            'ip_metadata_hash': web3.to_hex(web3.keccak(text="test-metadata-hash")),
+            'nft_metadata_uri': "test-nft-uri",
+            'nft_metadata_hash': web3.to_hex(web3.keccak(text="test-nft-metadata-hash"))
+        }
 
-#         response = story_client.IPAsset.register(
-#             nft_contract=MockERC721,
-#             token_id=token_id,
-#             ip_metadata=metadata,
-#             deadline=1000
-#         )
+        response = story_client.IPAsset.register(
+            nft_contract=MockERC721,
+            token_id=token_id,
+            ip_metadata=metadata,
+            deadline=1000
+        )
 
-#         assert 'txHash' in response
-#         assert isinstance(response['txHash'], str)
+        assert 'txHash' in response
+        assert isinstance(response['txHash'], str)
 
-#         assert response is not None
-#         assert 'ipId' in response
-#         assert response['ipId'] is not None
-#         assert isinstance(response['ipId'], str)
+        assert response is not None
+        assert 'ipId' in response
+        assert response['ipId'] is not None
+        assert isinstance(response['ipId'], str)
 
-# class TestIPAssetDerivatives:
-#     @pytest.fixture(scope="module")
-#     def non_commercial_license(self, story_client):
-#         license_register_response = story_client.License.registerNonComSocialRemixingPIL()
-#         no_commercial_license_terms_id = license_register_response['licenseTermsId']
-#         return no_commercial_license_terms_id
+class TestIPAssetDerivatives:
+    @pytest.fixture(scope="module")
+    def non_commercial_license(self, story_client):
+        license_register_response = story_client.License.registerNonComSocialRemixingPIL()
+        no_commercial_license_terms_id = license_register_response['licenseTermsId']
+        return no_commercial_license_terms_id
 
-#     @pytest.fixture(scope="module")
-#     def parent_ip_id(self, story_client, non_commercial_license):
-#         token_id = get_token_id(MockERC721, story_client.web3, story_client.account)
-#         response = story_client.IPAsset.register(
-#             nft_contract=MockERC721,
-#             token_id=token_id
-#         )
+    @pytest.fixture(scope="module")
+    def parent_ip_id(self, story_client, non_commercial_license):
+        token_id = get_token_id(MockERC721, story_client.web3, story_client.account)
+        response = story_client.IPAsset.register(
+            nft_contract=MockERC721,
+            token_id=token_id
+        )
 
-#         attach_license_response = story_client.License.attachLicenseTerms(response['ipId'], PIL_LICENSE_TEMPLATE, non_commercial_license)
+        attach_license_response = story_client.License.attachLicenseTerms(response['ipId'], PIL_LICENSE_TEMPLATE, non_commercial_license)
 
-#         return response['ipId']
+        return response['ipId']
 
-#     def test_register_derivative(self, story_client, child_ip_id, parent_ip_id, non_commercial_license):
-#         response = story_client.IPAsset.registerDerivative(
-#             child_ip_id=child_ip_id,
-#             parent_ip_ids=[parent_ip_id],
-#             license_terms_ids=[non_commercial_license],
-#             max_minting_fee=0,
-#             max_rts=5 * 10 ** 6,
-#             max_revenue_share=0,
-#         )
+    def test_register_derivative(self, story_client, child_ip_id, parent_ip_id, non_commercial_license):
+        response = story_client.IPAsset.registerDerivative(
+            child_ip_id=child_ip_id,
+            parent_ip_ids=[parent_ip_id],
+            license_terms_ids=[non_commercial_license],
+            max_minting_fee=0,
+            max_rts=5 * 10 ** 6,
+            max_revenue_share=0,
+        )
         
-#         assert response is not None
-#         assert 'txHash' in response
-#         assert response['txHash'] is not None
-#         assert isinstance(response['txHash'], str)
-#         assert len(response['txHash']) > 0
+        assert response is not None
+        assert 'txHash' in response
+        assert response['txHash'] is not None
+        assert isinstance(response['txHash'], str)
+        assert len(response['txHash']) > 0
 
-#     def test_registerDerivativeWithLicenseTokens(self, story_client, parent_ip_id, non_commercial_license):
-#         token_id = get_token_id(MockERC721, story_client.web3, story_client.account)
-#         child_response = story_client.IPAsset.register(
-#             nft_contract=MockERC721,
-#             token_id=token_id
-#         )
-#         child_ip_id = child_response['ipId']
+    def test_registerDerivativeWithLicenseTokens(self, story_client, parent_ip_id, non_commercial_license):
+        token_id = get_token_id(MockERC721, story_client.web3, story_client.account)
+        child_response = story_client.IPAsset.register(
+            nft_contract=MockERC721,
+            token_id=token_id
+        )
+        child_ip_id = child_response['ipId']
 
-#         license_token_response = story_client.License.mintLicenseTokens(
-#             licensor_ip_id=parent_ip_id, 
-#             license_template=PIL_LICENSE_TEMPLATE, 
-#             license_terms_id=non_commercial_license, 
-#             amount=1,
-#             receiver=account.address,
-#             max_minting_fee=0,
-#             max_revenue_share=1
-#         )
-#         licenseTokenIds = license_token_response['licenseTokenIds']
+        license_token_response = story_client.License.mintLicenseTokens(
+            licensor_ip_id=parent_ip_id, 
+            license_template=PIL_LICENSE_TEMPLATE, 
+            license_terms_id=non_commercial_license, 
+            amount=1,
+            receiver=account.address,
+            max_minting_fee=0,
+            max_revenue_share=1
+        )
+        licenseTokenIds = license_token_response['licenseTokenIds']
 
-#         response = story_client.IPAsset.registerDerivativeWithLicenseTokens(
-#             child_ip_id=child_ip_id,
-#             license_token_ids=licenseTokenIds,
-#             max_rts=5 * 10 ** 6
-#         )
+        response = story_client.IPAsset.registerDerivativeWithLicenseTokens(
+            child_ip_id=child_ip_id,
+            license_token_ids=licenseTokenIds,
+            max_rts=5 * 10 ** 6
+        )
         
-#         assert response is not None
-#         assert 'txHash' in response
-#         assert response['txHash'] is not None
-#         assert isinstance(response['txHash'], str)
-#         assert len(response['txHash']) > 0
+        assert response is not None
+        assert 'txHash' in response
+        assert response['txHash'] is not None
+        assert isinstance(response['txHash'], str)
+        assert len(response['txHash']) > 0
 
 class TestIPAssetMinting:
     @pytest.fixture(scope="module")

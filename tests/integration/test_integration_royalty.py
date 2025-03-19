a8924c7b9eae15963b21687b758c7d059946e461
bpolania <boris.polania@gmail.com>
2025-03-19 14:42:03 -0700

    Update test_integration_royalty.py

--- tests/integration/test_integration_royalty.py
+++ tests/integration/test_integration_royalty.py
@@ -75,8 +75,74 @@ class TestRoyalty:
 
         setup_royalty_vault(story_client, parent_ip_id, account)
 
-    def test_pay_royalty_on_behalf(self, story_client, parent_ip_id, child_ip_id, attach_and_register):
+    @pytest.fixture(scope="module")
+    def setup_ips_and_licenses(self, story_client):
+        """Setup parent and child IPs with proper license relationships"""
+        
+        parent_token_id = get_token_id(MockERC721, story_client.web3, story_client.account)
+        parent_ip_response = story_client.IPAsset.register(
+            nft_contract=MockERC721,
+            token_id=parent_token_id
+        )
+        parent_ip_id = parent_ip_response['ipId']
+        
+        child_token_id = get_token_id(MockERC721, story_client.web3, story_client.account)
+        child_ip_response = story_client.IPAsset.register(
+            nft_contract=MockERC721,
+            token_id=child_token_id
+        )
+        child_ip_id = child_ip_response['ipId']
+        
+        license_terms_response = story_client.License.registerCommercialRemixPIL(
+            default_minting_fee=100000,
+            currency=MockERC20,
+            commercial_rev_share=10,
+            royalty_policy=ROYALTY_POLICY
+        )
+        license_terms_id = license_terms_response['licenseTermsId']
+        
+        story_client.License.attachLicenseTerms(
+            ip_id=parent_ip_id,
+            license_template=PIL_LICENSE_TEMPLATE,
+            license_terms_id=license_terms_id
+        )
+        
+        story_client.IPAsset.registerDerivative(
+            child_ip_id=child_ip_id,
+            parent_ip_ids=[parent_ip_id],
+            license_terms_ids=[license_terms_id],
+            max_minting_fee=0,
+            max_rts=0,
+            max_revenue_share=0,
+        )
+        
+        mint_tokens(
+            erc20_contract_address=MockERC20, 
+            web3=web3, 
+            account=account, 
+            to_address=account.address, 
+            amount=100000 * 10 ** 6
+        )
+        
+        approve(
+            erc20_contract_address=MockERC20, 
+            web3=web3, 
+            account=account, 
+            spender_address=ROYALTY_MODULE, 
+            amount=100000 * 10 ** 6
+        )
+        
+        return {
+            'parent_ip_id': parent_ip_id,
+            'child_ip_id': child_ip_id,
+            'license_terms_id': license_terms_id
+        }
+
+    def test_pay_royalty_on_behalf(self, story_client, setup_ips_and_licenses):
         """Test paying royalty on behalf of a payer IP to a receiver IP"""
+        parent_ip_id = setup_ips_and_licenses['parent_ip_id']
+        child_ip_id = setup_ips_and_licenses['child_ip_id']
+        
         response = story_client.Royalty.payRoyaltyOnBehalf(
             receiver_ip_id=parent_ip_id,
             payer_ip_id=child_ip_id,

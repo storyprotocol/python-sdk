82efad51dd5ca427ed244bf5e0e4375df93c5572
Andrew Chung <aandrewchung@gmail.com>
2025-06-04 09:49:58 -0700

    Removed unused set permissions fns from auto generated client

--- src/story_protocol_python_sdk/abi/AccessController/AccessController_client.py
+++ src/story_protocol_python_sdk/abi/AccessController/AccessController_client.py
@@ -28,18 +28,6 @@ class AccessControllerClient:
     def build_setAllPermissions_transaction(self, ipAccount, signer, permission, tx_params):
         return self.contract.functions.setAllPermissions(ipAccount, signer, permission).build_transaction(tx_params)
     
-    def setBatchPermissions(self, permissions):
-        return self.contract.functions.setBatchPermissions(permissions).transact()
-        
-    def build_setBatchPermissions_transaction(self, permissions, tx_params):
-        return self.contract.functions.setBatchPermissions(permissions).build_transaction(tx_params)
-    
-    def setPermission(self, ipAccount, signer, to, func, permission):
-        return self.contract.functions.setPermission(ipAccount, signer, to, func, permission).transact()
-        
-    def build_setPermission_transaction(self, ipAccount, signer, to, func, permission, tx_params):
-        return self.contract.functions.setPermission(ipAccount, signer, to, func, permission).build_transaction(tx_params)
-    
     def setTransientPermission(self, ipAccount, signer, to, func, permission):
         return self.contract.functions.setTransientPermission(ipAccount, signer, to, func, permission).transact()
         

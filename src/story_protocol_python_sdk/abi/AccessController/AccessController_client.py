b2f96e6262ace304d963f82c03a3745449240f71
Andrew Chung <aandrewchung@gmail.com>
2025-06-04 09:46:04 -0700

    Auto generated DerivativeWorkflows fns

--- src/story_protocol_python_sdk/abi/AccessController/AccessController_client.py
+++ src/story_protocol_python_sdk/abi/AccessController/AccessController_client.py
@@ -28,11 +28,17 @@ class AccessControllerClient:
     def build_setAllPermissions_transaction(self, ipAccount, signer, permission, tx_params):
         return self.contract.functions.setAllPermissions(ipAccount, signer, permission).build_transaction(tx_params)
     
-    def setTransientBatchPermissions(self, permissions):
-        return self.contract.functions.setTransientBatchPermissions(permissions).transact()
+    def setBatchPermissions(self, permissions):
+        return self.contract.functions.setBatchPermissions(permissions).transact()
         
-    def build_setTransientBatchPermissions_transaction(self, permissions, tx_params):
-        return self.contract.functions.setTransientBatchPermissions(permissions).build_transaction(tx_params)
+    def build_setBatchPermissions_transaction(self, permissions, tx_params):
+        return self.contract.functions.setBatchPermissions(permissions).build_transaction(tx_params)
+    
+    def setPermission(self, ipAccount, signer, to, func, permission):
+        return self.contract.functions.setPermission(ipAccount, signer, to, func, permission).transact()
+        
+    def build_setPermission_transaction(self, ipAccount, signer, to, func, permission, tx_params):
+        return self.contract.functions.setPermission(ipAccount, signer, to, func, permission).build_transaction(tx_params)
     
     def setTransientPermission(self, ipAccount, signer, to, func, permission):
         return self.contract.functions.setTransientPermission(ipAccount, signer, to, func, permission).transact()

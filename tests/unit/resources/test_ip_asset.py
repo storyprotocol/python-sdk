eccee640d05d1a8b1b3b50d88fc6f2474d7d29a9
Bonnie <bonnie@57blocks.com>
2025-08-12 19:30:39 +0800

    Add unit tests including  mint and register_ip_and_attach_pil_terms

--- tests/unit/resources/test_ip_asset.py
+++ tests/unit/resources/test_ip_asset.py
@@ -5,7 +5,15 @@ import pytest
 from story_protocol_python_sdk.resources.IPAsset import IPAsset
 from story_protocol_python_sdk.utils.constants import ZERO_HASH
 from story_protocol_python_sdk.utils.derivative_data import DerivativeDataInput
-from tests.unit.fixtures.data import ADDRESS, CHAIN_ID, IP_ID, TX_HASH
+from story_protocol_python_sdk.utils.ip_metadata import IPMetadata
+from tests.unit.fixtures.data import (
+    ADDRESS,
+    CHAIN_ID,
+    IP_ID,
+    LICENSE_TERMS,
+    LICENSING_CONFIG,
+    TX_HASH,
+)
 
 
 @pytest.fixture(scope="class")
@@ -39,7 +47,21 @@ def mock_is_registered(ip_asset):
 def mock_parse_ip_registered_event(ip_asset):
     def _mock():
         return patch.object(
-            ip_asset, "_parse_tx_ip_registered_event", return_value={"ip_id": IP_ID}
+            ip_asset,
+            "_parse_tx_ip_registered_event",
+            return_value={"ip_id": IP_ID, "token_id": 1},
+        )
+
+    return _mock
+
+
+@pytest.fixture(scope="class")
+def mock_parse_tx_license_terms_attached_event(ip_asset):
+    def _mock():
+        return patch.object(
+            ip_asset,
+            "_parse_tx_license_terms_attached_event",
+            return_value=[1, 2],
         )
 
     return _mock
@@ -172,3 +194,151 @@ class TestRegisterDerivativeIp:
                 )
                 assert result["tx_hash"] == TX_HASH.hex()
                 assert result["ip_id"] == IP_ID
+
+
+class TestMint:
+    def test_mint_successful(self, ip_asset):
+        result = ip_asset.mint(
+            nft_contract=ADDRESS,
+            to_address=ADDRESS,
+            metadata_uri="",
+            metadata_hash=ZERO_HASH,
+        )
+        assert result == f"0x{TX_HASH.hex()}"
+
+    def test_mint_failed_transaction(self, ip_asset):
+        with patch.object(ip_asset.web3.eth, "send_raw_transaction") as mock_send:
+            mock_send.side_effect = Exception("Transaction failed")
+            with pytest.raises(Exception, match="Transaction failed"):
+                ip_asset.mint(
+                    nft_contract=ADDRESS,
+                    to_address=ADDRESS,
+                    metadata_uri="",
+                    metadata_hash=ZERO_HASH,
+                    allow_duplicates=False,
+                )
+
+
+class TestRegisterIpAndAttachPilTerms:
+    def test_token_id_is_already_registered(
+        self, ip_asset, mock_get_ip_id, mock_is_registered
+    ):
+        with mock_get_ip_id(), mock_is_registered(True):
+            with pytest.raises(
+                ValueError, match="The NFT with id 3 is already registered as IP."
+            ):
+                ip_asset.register_ip_and_attach_pil_terms(
+                    nft_contract=ADDRESS,
+                    token_id=3,
+                    license_terms_data=[],
+                )
+
+    def test_royalty_policy_commercial_rev_share_is_less_than_0(
+        self, ip_asset, mock_get_ip_id, mock_is_registered
+    ):
+        with mock_get_ip_id(), mock_is_registered():
+            with pytest.raises(
+                ValueError, match="commercial_rev_share should be between 0 and 100."
+            ):
+                ip_asset.register_ip_and_attach_pil_terms(
+                    nft_contract=ADDRESS,
+                    token_id=3,
+                    license_terms_data=[
+                        {
+                            "terms": {
+                                **LICENSE_TERMS,
+                                "commercial_rev_share": -1,
+                            },
+                        }
+                    ],
+                )
+
+    def test_transaction_to_be_called_with_correct_parameters(
+        self,
+        ip_asset: IPAsset,
+        mock_get_ip_id,
+        mock_is_registered,
+        mock_parse_ip_registered_event,
+        mock_parse_tx_license_terms_attached_event,
+        mock_signature_related_methods,
+    ):
+        with mock_get_ip_id(), mock_is_registered(), mock_parse_ip_registered_event(), mock_parse_tx_license_terms_attached_event(), mock_signature_related_methods():
+            with patch.object(
+                ip_asset.license_attachment_workflows_client,
+                "build_registerIpAndAttachPILTerms_transaction",
+            ) as mock_build_registerIpAndAttachPILTerms_transaction:
+
+                ip_asset.register_ip_and_attach_pil_terms(
+                    nft_contract=ADDRESS,
+                    token_id=3,
+                    license_terms_data=[
+                        {
+                            "terms": LICENSE_TERMS,
+                            "licensing_config": LICENSING_CONFIG,
+                        }
+                    ],
+                )
+            call_args = mock_build_registerIpAndAttachPILTerms_transaction.call_args[0]
+            assert call_args[0] == ADDRESS
+            assert call_args[1] == 3
+            assert call_args[2] == IPMetadata.from_input().get_validated_data()
+            assert call_args[3] == [
+                {
+                    "terms": {
+                        "transferable": True,
+                        "royaltyPolicy": "0x1234567890123456789012345678901234567890",
+                        "defaultMintingFee": 10,
+                        "expiration": 100,
+                        "commercialUse": True,
+                        "commercialAttribution": True,
+                        "commercializerChecker": True,
+                        "commercializerCheckerData": b"mock_bytes",
+                        "commercialRevShare": 19000000,
+                        "commercialRevCeiling": 0,
+                        "derivativesAllowed": True,
+                        "derivativesAttribution": True,
+                        "derivativesApproval": True,
+                        "derivativesReciprocal": True,
+                        "derivativeRevCeiling": 100,
+                        "currency": "0x1234567890123456789012345678901234567890",
+                        "uri": "https://example.com",
+                    },
+                    "licensingConfig": {
+                        "isSet": True,
+                        "mintingFee": 10,
+                        "hookData": b"mock_bytes",
+                        "licensingHook": "0x1234567890123456789012345678901234567890",
+                        "commercialRevShare": 10000000,
+                        "disabled": False,
+                        "expectMinimumGroupRewardShare": 10000000,
+                        "expectGroupRewardPool": "0x1234567890123456789012345678901234567890",
+                    },
+                }
+            ]
+
+    def test_success(
+        self,
+        ip_asset: IPAsset,
+        mock_get_ip_id,
+        mock_is_registered,
+        mock_parse_ip_registered_event,
+        mock_signature_related_methods,
+        mock_parse_tx_license_terms_attached_event,
+    ):
+        with mock_get_ip_id(), mock_is_registered(), mock_parse_ip_registered_event(), mock_parse_tx_license_terms_attached_event(), mock_signature_related_methods():
+            result = ip_asset.register_ip_and_attach_pil_terms(
+                nft_contract=ADDRESS,
+                token_id=3,
+                license_terms_data=[
+                    {
+                        "terms": LICENSE_TERMS,
+                        "licensing_config": LICENSING_CONFIG,
+                    }
+                ],
+            )
+            assert result == {
+                "tx_hash": TX_HASH.hex(),
+                "ip_id": IP_ID,
+                "license_terms_ids": [1, 2],
+                "token_id": 1,
+            }
